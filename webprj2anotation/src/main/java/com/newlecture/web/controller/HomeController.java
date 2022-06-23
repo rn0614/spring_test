11d9dfea554636ac5229a7706d5641c6097626fa
Koo SangMo <rn0614@naver.com>
2022-06-23 23:57:28 +0900

    ssss

--- webprj2anotation/src/main/java/com/newlecture/web/controller/HomeController.java
+++ webprj2anotation/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,13 +1,37 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController{
-	// resourceViewResolver로 index.jsp를 찾고 있음
+	
+	// 문자열로 출력 , Servlet response
+	//@RequestMapping("index")
+	public void index1(HttpServletResponse response) throws IOException {
+		PrintWriter out;
+		
+		out = response.getWriter();
+		out.println("hello");
+	}
+	
+	// 위와같이 전달하는 String이 그냥 문자열일 경우 @ResponseBody 사용
 	@RequestMapping("index")
+	@ResponseBody
+	public String index2() {
+		return "Hello Index";
+	}
+	
+	
+	// resourceViewResolver로 index.jsp를 찾고 있음
+	//@RequestMapping("index")
 	public String index() {
 		//리솔빙됨
 		return "root.index";
--- webprj2anotation/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ webprj2anotation/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -17,8 +18,13 @@ public class NoticeController {
 	@Autowired
 	private NoticeService noticeService;
 	
+	// 그냥 String만 넣어도 HttpServletRequest와 같음 getParameter까지 한 상태 / 디폴트 1에 / int로 파싱가능 / name 대신 value 사용가능
 	@RequestMapping("list")
-	public String list() throws ClassNotFoundException, SQLException {
+	//public String list(@RequestParam(name="p",defaultValue = "1") int page) throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name="p", required=false) Integer page) throws ClassNotFoundException, SQLException {
+		//String p= request.getParameter("p");
+		System.out.println(page);
+		
 		List<Notice> list = noticeService.getList(1, "TITLE", "");
 		
 		return "notice.list";
